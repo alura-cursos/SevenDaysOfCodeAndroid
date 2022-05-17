a847fbd225e467ed0f162689828f68b8f54529aa
Alex Felipe <alexfelipevieira@gmail.com>
2022-05-17 13:45:20 -0300

    Day 1

---
+++ app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -0,0 +1,45 @@
+package br.com.alura.devhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.alura.devhub.ui.theme.DevHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DevHubTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ProfileScreen()
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreen() {
+    Column {
+        Image(painterResource(id = R.drawable.alex_avatar), contentDescription = "profile pic")
+        Text("Alex Felipe")
+        Text("alexfelipe")
+        Text("Instructor and Software Developer at @alura-cursos")
+    }
+}
