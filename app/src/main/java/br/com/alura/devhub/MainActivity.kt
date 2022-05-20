2bb3e7bc4ead355256e017d691b10972a48d76c4
Alex Felipe <alexfelipevieira@gmail.com>
2022-05-20 18:17:49 -0300

    Day 2

--- app/src/main/java/br/com/alura/devhub/MainActivity.kt
+++ app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.devhub.ui.theme.DevHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,9 +47,62 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ProfileScreen() {
     Column {
-        Image(painterResource(id = R.drawable.alex_avatar), contentDescription = "profile pic")
-        Text("Alex Felipe")
-        Text("alexfelipe")
-        Text("Instructor and Software Developer at @alura-cursos")
+        val boxHeight = remember {
+            150.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Color(0xFF2d333b), shape = RoundedCornerShape(
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
+                    )
+                )
+                .height(boxHeight)
+        ) {
+            Image(
+                painterResource(id = R.drawable.alex_avatar),
+                contentDescription = "profile pic",
+                modifier = Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape),
+            )
+        }
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+        Column(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Alex Felipe",
+                fontSize = 32.sp
+            )
+            Text(
+                "alexfelipe",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Text(
+            "Instructor and Software Developer at @alura-cursos",
+            Modifier
+                .padding(
+                    start = 8.dp,
+                    bottom = 8.dp,
+                    end = 8.dp
+                )
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
+
+
